78293e350e816a160323491663f2cbade41da020
Sabrina Jones <sabrina.jones87@gmail.com>
2018-01-10 18:53:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function getCommits(el) {
     const name = el.dataset.repo;
     const req = new XMLHttpRequest();
     req.addEventListener("load", displayCommits);
-    req.open("GET", '/repos/" + el.dataset.username + "/" + repoName + "/commit');
+    req.open("GET", 'https://api.github.com/repos/' + name + '/commits');
     req.send();
 }
 
